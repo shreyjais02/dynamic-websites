2b6215fefa33ff260d7184d6d401429b80ef3e32
Shreyash Jaiswal <shray.pvt@gmail.com>
2026-04-29 22:10:08 +0530

    .

---
+++ 2_traffic_light/traffic_light.js
@@ -0,0 +1,24 @@
+function stopbtn() {
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124";
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+}
+function readybtn() {
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948";
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+}
+function gobtn() {
+    document.getElementById("goButton").style.backgroundColor = "#199473";
+    document.getElementById("goLight").style.backgroundColor = "#199473";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+}
