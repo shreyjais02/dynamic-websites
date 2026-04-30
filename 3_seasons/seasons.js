dd7f634334c64b087acd24940fd95029d78497ba
Shreyash Jaiswal <shray.pvt@gmail.com>
2026-04-30 22:01:23 +0530

    .

---
+++ 3_seasons/seasons.js
@@ -0,0 +1,31 @@
+function spring() {
+  document.getElementById("seasonSmallImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-spring-xs-img.png";
+
+  document.getElementById("seasonMediumImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-spring-md-img.png";
+}
+
+function summer() {
+  document.getElementById("seasonSmallImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-summer-xs-img.png";
+
+  document.getElementById("seasonMediumImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-summer-md-img.png";
+}
+
+function autumn() {
+  document.getElementById("seasonSmallImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-autumn-xs-img.png";
+
+  document.getElementById("seasonMediumImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-autumn-md-img.png";
+}
+
+function winter() {
+  document.getElementById("seasonSmallImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-winter-xs-img.png";
+
+  document.getElementById("seasonMediumImage").src =
+    "https://s3.ap-south-1.amazonaws.com/new-assets.ccbp.in/frontend/loading-data/niat_dynamic_webapps/niat_coding_questions/seasons-switcher-winter-md-img.png";
+}
