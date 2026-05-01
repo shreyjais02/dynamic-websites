57e5a4955ce57c502df88cf9e51933219141f1b4
Shreyash Jaiswal <shray.pvt@gmail.com>
2026-05-01 21:49:38 +0530

    .

---
+++ 4_web_technologies/web_technologies.js
@@ -0,0 +1,4 @@
+function manipulateStyles() {
+    document.getElementById("headingElement").textContent = "4.0 Technologies";
+    document.getElementById("headingElement").style.color = 'Blue';
+}
