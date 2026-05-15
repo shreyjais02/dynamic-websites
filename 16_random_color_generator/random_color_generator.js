9c71d41e41ce688b14a001254a76b5d8497a88bc
Shreyash Jaiswal <shray.pvt@gmail.com>
2026-05-15 22:03:46 +0530

    .

---
+++ 16_random_color_generator/random_color_generator.js
@@ -0,0 +1,15 @@
+let bgColorsArray = [
+    "#e75d7c",
+    "#b16cef",
+    "#53cca4",
+    "#efc84d",
+    "#628ef0",
+    "#184b73",
+    "#883e7f",
+    "#ed048b",
+];
+
+function onChangeBgColor() {
+    let randomIndex = Math.floor(Math.random() * bgColorsArray.length);
+    document.getElementById("bgContainer").style.backgroundColor = bgColorsArray[randomIndex];
+}
